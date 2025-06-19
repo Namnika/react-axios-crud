50b607c3c738e23a36914caef6e7a0e73e2610c8
Namnika <janbandhunamnika@gmail.com>
2025-06-19 12:15:06 +0530

    solving issues of editing field

--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
-import logo from './logo.svg';
+import React, { use, useEffect, useState } from 'react';
+import {
+  getUsers, createUser, updateUser, deleteUser
+} from "./services/api"
+
 import './App.css';
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+  const [form, setForm] = useState({
+    name: '',
+    email: ''
+  })
+  const [editingId, setEditingId] = useState(null);
+  const [inputValue, setInputValue] = useState('')
+  const [isEditingInput, setEditingInput] = useState(false);
+
+
+  const fetchUsers = async () => {
+    const res = await getUsers();
+    setUsers(res.data)
+  }
+
+  useEffect(() => {
+    fetchUsers()
+  }, [])
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (editingId) {
+      await updateUser(editingId, form);
+      setEditingId(null)
+    } else {
+      await createUser(form)
+    }
+    setForm({ name: '', email: '' })
+
+    fetchUsers();
+  }
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value })
+  }
+
+  const handleEditClick = (id) => {
+    setEditingId(id)
+    setEditingInput(true)
+    
+
+  }
+
+  const handleDelete = async (id) => {
+    await deleteUser(id)
+    fetchUsers();
+  }
+
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>User Management</h2>
+      <form onSubmit={handleSubmit} >
+        <input name='name' value={form.name} onChange={handleChange} placeholder='Enter your name' required />
+        <input name='email' value={form.email} onChange={handleChange} placeholder='Enter your email' required />
+        <button type='submit'>Add User </button>
+        <ul>
+          {users.map((user, index) => {
+            return <li key={user.id}>
+              {isEditingInput && editingId === user.id ?
+                <>
+                  <input name='name' value={user.name} onChange={handleChange} />
+                  <input name='email' value={form.email} onChange={(e) => setInputValue(e.target.value)} />
+                  <button onClick={handleSubmit}>Save</button>
+                </> :
+
+                <span>{user.name} ({user.email})</span>
+              }
+              <button onClick={() => handleEditClick(user.id)}>Edit</button>
+              <button onClick={() => handleDelete(user.id)} >Delete</button>
+            </li>
+
+          })}
+        </ul>
+      </form>
     </div>
   );
 }
--- src/services/api.js
+++ src/services/api.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const BASE_API_URL = "http://localhost:3001/users";
 
 export const getUsers = () => axios.get(BASE_API_URL);
-export const getUser = (id) => axios.get(`${BASE_API_URL}/users/:${id}`)
-export const createUser = (user) => axios.post(`${BASE_API_URL}/${user}`)
-export const updateUser = (id, user) => axios.put(`${BASE_API_URL}/${user}/:${id}`)
+export const getUser = (id) => axios.get(`${BASE_API_URL}/:${id}`)
+export const createUser = (user) => axios.post(BASE_API_URL, user)
+export const updateUser = (id, user) => axios.put(`${BASE_API_URL}/${id}`, user)
 export const deleteUser = (id) => axios.delete(`${BASE_API_URL}/${id}`)
 
